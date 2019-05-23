ce9b71b76ac7bf238607e0ddb592ad061e81bfd7
NguyenCuong <thecuongthehieu@gmail.com>
2019-05-23 14:35:25 +0700

    lab11 Graphs

--- Assignments/lab11/lab11/graphs/MazeBreadthFirstPaths.java
+++ Assignments/lab11/lab11/graphs/MazeBreadthFirstPaths.java
@@ -1,5 +1,7 @@
 package lab11.graphs;
 
+import edu.princeton.cs.algs4.Queue;
+
 /**
  *  @author Josh Hug
  */
@@ -9,21 +11,54 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private int s;
+    private int t;
+    private boolean targetFound = false;
+    private Maze maze;
 
     public MazeBreadthFirstPaths(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
         // Add more variables here!
+        maze = m;
+        s = maze.xyTo1D(sourceX, sourceY);
+        t = maze.xyTo1D(targetX, targetY);
+        distTo[s] = 0;
+        edgeTo[s] = s;
     }
 
     /** Conducts a breadth first search of the maze starting at the source. */
-    private void bfs() {
+    private void bfs(int v) {
         // TODO: Your code here. Don't forget to update distTo, edgeTo, and marked, as well as call announce()
-    }
+        Queue<Integer> queue = new Queue<>();
+        queue.enqueue(v);
 
+        while (!queue.isEmpty()) {
+
+            int w = queue.dequeue();
+            marked[w] = true;
+            announce();
+
+            if (w == t) {
+                targetFound = true;
+            }
+
+            if (targetFound) {
+                return;
+            }
+
+            for (int u : maze.adj(w)) {
+                if (!marked[u]) {
+                    edgeTo[u] = w;
+                    distTo[u] = distTo[w] + 1;
+                    queue.enqueue(u);
+                }
+            }
+        }
+    }
 
     @Override
     public void solve() {
-        // bfs();
+        bfs(s);
     }
 }
 
--- Assignments/lab11/lab11/graphs/MazeCycles.java
+++ Assignments/lab11/lab11/graphs/MazeCycles.java
@@ -9,16 +9,74 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private int start;
+    private int target;
+    private boolean circleFound = false;
+    private int endVertexOfCircle = Integer.MAX_VALUE;
+    private boolean hasFullCircle = false;
+    private Maze maze;
+    private int[] parent;
+
 
     public MazeCycles(Maze m) {
         super(m);
+        maze = m;
+        parent = new int[maze.V()];
+
+        for (int i = 0; i < maze.V(); ++i) {
+            parent[i] = i;
+            distTo[i] = 0;
+            edgeTo[i] = i;
+        }
+
+        int startX = 1;
+        int startY = 1;
+        int targetX = maze.N();
+        int targetY = maze.N();
+
+        start = maze.xyTo1D(startX, startY);
+        target = maze.xyTo1D(targetX, targetY);
     }
 
     @Override
     public void solve() {
         // TODO: Your code here!
+        for (int u = start; u <= target; ++u) {
+            if (circleFound) {
+                break;
+            }
+            if (!marked[u]) {
+                dfs(u);
+            }
+        }
     }
 
     // Helper methods go here
+    private void dfs(int u) {
+        marked[u] = true;
+
+        for (int v : maze.adj(u)) {
+            if (!marked[v]) {
+                parent[v] = u;
+                distTo[v] = distTo[u] + 1;
+                dfs(v);
+                if (circleFound) {
+                    if (v != endVertexOfCircle && !hasFullCircle) {
+                        edgeTo[v] = parent[v];
+                    } else {
+                        hasFullCircle = true;
+                    }
+                    break;
+                }
+            } else if (parent[u] != v) {
+                circleFound = true;
+                endVertexOfCircle = v;
+                parent[v] = u;
+                edgeTo[v] = parent[v];
+                break;
+            }
+        }
+        announce();
+    }
 }
 
