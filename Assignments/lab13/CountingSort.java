fcd1ff0518e33d90d499e9375f2ba56a32966dab
NguyenCuong <thecuongthehieu@gmail.com>
2019-05-24 15:01:02 +0700

    lab13 Radix Sorts

--- Assignments/lab13/CountingSort.java
+++ Assignments/lab13/CountingSort.java
@@ -67,6 +67,28 @@ public class CountingSort {
      */
     public static int[] betterCountingSort(int[] arr) {
         // TODO make counting sort work with arrays containing negative numbers.
-        return null;
+        int[] positiveArr = new int[arr.length];
+        int min = Integer.MAX_VALUE;
+
+        for (int i = 0; i < arr.length; ++i) {
+            min = min < arr[i] ? min : arr[i];
+        }
+
+        if (min >= 0) {
+            return naiveCountingSort(arr);
+        }
+
+        for (int i = 0; i < positiveArr.length; ++i) {
+            positiveArr[i] = arr[i] - min;
+        }
+
+        int[] sortedPositiveArr = naiveCountingSort(positiveArr);
+        int[] sortedArr = new int[sortedPositiveArr.length];
+
+        for (int i = 0; i < sortedArr.length; ++i) {
+            sortedArr[i] = sortedPositiveArr[i] + min;
+        }
+
+        return sortedArr;
     }
 }
--- Assignments/lab13/RadixSort.java
+++ Assignments/lab13/RadixSort.java
@@ -4,6 +4,11 @@
  * @author Akhil Batra, Alexander Hwang
  *
  */
+
+import edu.princeton.cs.algs4.Queue;
+
+import java.util.ArrayList;
+
 public class RadixSort {
     /**
      * Does LSD radix sort on the passed in array with the following restrictions:
@@ -15,9 +20,32 @@ public class RadixSort {
      *
      * @return String[] the sorted array
      */
+    private static final int RADIX_VALUE = 256;
+
     public static String[] sort(String[] asciis) {
         // TODO: Implement LSD Sort
-        return null;
+        String[] sameLengthArr = new String[asciis.length];
+        int maxLength = Integer.MIN_VALUE;
+
+        for (String str : asciis) {
+            maxLength = maxLength > str.length() ? maxLength : str.length();
+        }
+
+        for (int i = 0; i < asciis.length; ++i) {
+            sameLengthArr[i] = asciis[i];
+
+            int len = asciis[i].length();
+            for (int k = 1; k <= (maxLength - len); ++k) {
+                sameLengthArr[i] += (char)0;
+            }
+
+        }
+
+        for (int id = maxLength - 1; id >= 0; --id) {
+            sortHelperLSD(sameLengthArr, id);
+        }
+
+        return sameLengthArr;
     }
 
     /**
@@ -28,7 +56,23 @@ public class RadixSort {
      */
     private static void sortHelperLSD(String[] asciis, int index) {
         // Optional LSD helper method for required LSD radix sort
-        return;
+        ArrayList<Queue<String>> queueList = new ArrayList<>();
+        for (int i = 1; i <= RADIX_VALUE; ++i) {
+            queueList.add(new Queue<>());
+        }
+
+        for (String str : asciis) {
+            int bucketNum = (int) str.charAt(index);
+            queueList.get(bucketNum).enqueue(str);
+        }
+
+        int i = 0;
+        for (Queue<String> queue : queueList) {
+            while (!queue.isEmpty()) {
+                asciis[i] = queue.dequeue();
+                ++i;
+            }
+        }
     }
 
     /**
@@ -44,5 +88,22 @@ public class RadixSort {
     private static void sortHelperMSD(String[] asciis, int start, int end, int index) {
         // Optional MSD helper method for optional MSD radix sort
         return;
+    }
+
+    public static void main(String[] args) {
+        String[] arr = new String[5];
+
+        arr[3] = new String("bcd");
+        arr[4] = new String("d");
+        arr[0] = new String("a");
+        arr[2] = new String("b");
+        arr[1] = new String("ab");
+
+
+        String[] sorted = sort(arr);
+
+        for (String st : sorted) {
+            System.out.println(st);
+        }
     }
 }
